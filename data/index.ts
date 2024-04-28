a72669f7340c46c5239667c14e5c54c85a5a0c58
nthapa000 <nishantthapa0000@gmail.com>
2024-04-28 22:42:06 +0530

    types

---
+++ data/index.ts
@@ -0,0 +1,14 @@
+export const parameters = [
+    "Average_Temperature",
+    "CloudCover",
+    "DiurnalTemperatureRange",
+    "GroundFrostFrequency",
+    "MaximumTemperature",
+    "MinimumTemperature",
+    "PotentialEvapotranspiration",
+    "Precipitation",
+    "Precipitation_2004_2011",
+    "ReferenceCropEvapotranspiration",
+    "VapourPressure",
+    "WetDayFrequency",
+  ];
---
+++ types/index.ts
@@ -0,0 +1,13 @@
+export interface State {
+    State: string;
+    stateid: number;
+}
+
+export interface District {
+    Distict: string;
+    districtid: number;
+}
+
+export interface Year {
+    year_val: number;
+}
