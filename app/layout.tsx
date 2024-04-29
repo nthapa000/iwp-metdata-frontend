b78bc1e02cbaeb48164477b79dd7a97e60f56fb2
nthapa000 <nishantthapa0000@gmail.com>
2024-04-29 20:45:35 +0530

    Footer removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Header />
+      <Header /> 
       <Suspense>
         {children}
         </Suspense>
-      <Footer />
+      {/* <Footer /> */}
       </body>
     </html>
   );
